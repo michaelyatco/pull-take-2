d8961d969776f126ac05373c8f301c72b3e1ed1b
Samantha Morco <samanthamorco@gmail.com>
2017-03-09 18:39:12 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Cheese.create(name: "cheddar", expiration_date: 10.days.from_now)
+Cheese.create(name: "swiss", expiration_date: 10.days.from_now)
+Cheese.create(name: "bleu", expiration_date: 10.days.from_now)
+Cheese.create(name: "american", expiration_date: 10.days.from_now)
